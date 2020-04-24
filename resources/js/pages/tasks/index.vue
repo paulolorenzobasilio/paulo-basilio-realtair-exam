ebf7cd2842a1632bb6ce0699966953ba5dca6303
Paulo Basilio <paulolorenzobasilio@yahoo.com>
2020-04-24 11:38:09 +0800

    added checkbox for masking as done

--- resources/js/pages/tasks/index.vue
+++ resources/js/pages/tasks/index.vue
@@ -2,30 +2,37 @@
   <card title="Tasks">
     <router-link :to="{ name: 'tasks.form'}" role="button" class="btn btn-primary">Create</router-link>
 
-    <table class="table mt-2">
+    <table class="table table-borderless mt-2">
       <thead>
         <tr>
-          <th scope="col">#</th>
+          <th scope="col"></th>
           <th scope="col">Title</th>
           <th scope="col">Description</th>
           <th scope="col">Actions</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(task, index) in tasks" :key="task.id">
-          <th scope="row">{{ index + 1 }}</th>
+        <tr v-for="(task) in tasks" :key="task.id">
+          <th scope="row">
+            <div class="form-check">
+              <input
+                v-if="!isDone(task)"
+                class="form-check-input"
+                type="checkbox"
+                @click="markAsDone(task.id)"
+              />
+            </div>
+          </th>
           <td v-bind:class="{'strike-text': isDone(task)}">{{ task.title }}</td>
           <td v-bind:class="{'strike-text': isDone(task)}">{{ task.description }}</td>
           <td>
             <div class="btn-group">
-              <template v-if="!isDone(task)">
-                <button type="button" class="btn btn-primary" @click="markAsDone(task.id)">Done?</button>
-                <router-link
-                  :to="{ name: 'tasks.show', params: {id: task.id} }"
-                  role="button"
-                  class="btn btn-secondary"
-                >Update</router-link>
-              </template>
+              <router-link
+                v-if="!isDone(task)"
+                :to="{ name: 'tasks.show', params: {id: task.id} }"
+                role="button"
+                class="btn btn-secondary"
+              >Update</router-link>
               <button type="button" class="btn btn-danger" @click="deleteTask(task.id)">Delete</button>
             </div>
           </td>
@@ -74,7 +81,7 @@ export default {
 
     async deleteTask(id) {
       await axios.delete(`/api/task/${id}`);
-      
+
       this.tasks = this.tasks.filter(task => task.id !== id);
     }
   }
